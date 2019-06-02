b9f026ccd4b5328031208ad6870d1ef2e040dece
hoytross <hoytross@seattleu.edu>
2019-06-02 00:54:51 -0700

    init commit

---
+++ TestingApp_Pizza/TestingApp_Pizza/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestingApp_Pizza.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
